d84949e60b452abb699f93577a8906d2327ca055
Franco <fcuppari@fi.uba.ar>
2023-03-25 18:11:31 -0300

    primes tests passed

--- primes.c
+++ primes.c
@@ -1,102 +1,102 @@
-//
-// Copyright [Cuppari, Franco; 2023]
-//
-#include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
-#include <time.h>
-#include <wait.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include <stdbool.h>
+#include <time.h>
+#include <sys/wait.h>
 
-void erastotenes(int *fds);
+void eratostenes(int *fds);
 
 void
-erastotenes(int *fds)
+eratostenes(int *fds)
 {
-	int prime = 0;
-	close(fds[1]);  //  No escribo en este pipe.
-	int ret = read(fds[0], &prime, sizeof(prime));
-	if (ret < 0) {
-		perror("Error en la lectura");
-		exit(-1);
-	} else if (ret == 0) {  //  EOF
-		close(fds[0]);
-		exit(-1);
-	}
-	printf("primo: %d\n", prime);
-	int fds_bis[2];
-	int result_bis = pipe(fds_bis);
-	if (result_bis < 0) {
-		perror("Error en pipe");
-	}
-	int f = fork();
-	if (f < 0) {
-		perror("Error en fork");
-		exit(-1);
-	}
-	if (f == 0) {  //  Hijo
-		close(fds[0]);
-		erastotenes(fds_bis);
-		exit(0);
-	} else {
-		close(fds_bis[0]);  //  Cierro lectura
-		_Bool closed = false;
-		while (!closed) {
-			int num = 0;
-			int reading = read(fds[0], &num, sizeof(num));
-			if (reading < 0) {
-				perror("Error en la lectura");
-				exit(-1);
-			} else if (reading == 0) {
-				//  EOF
-				closed = true;
-				close(fds[0]);
-			}
-			if (num % prime != 0) {
-				if (write(fds_bis[1], &num, sizeof(num)) < 0) {
-					perror("Error en la escritura");
-					exit(-1);
-				}
-			}
-		}
-	}
-	close(fds_bis[1]);
-	waitpid(f, NULL, 0);
-	exit(0);
+    int prime = 0;
+    close(fds[1]);
+    int r = read(fds[0], &prime, sizeof(prime));
+    if (r < 0) {
+        perror("Error en la lectura");
+        exit(-1);
+    } else if (r == 0) {
+        close(fds[0]);
+        exit(-1);
+    }
+
+    printf("primo %d\n", prime);
+
+    int fds_bis[2];
+    int r1 = pipe(fds_bis);
+    if (r1 < 0) {
+        perror("Error en pipe");
+    }
+    int i = fork();
+    if (i < 0) {
+        perror("Error en fork");
+        exit(-1);
+    }
+    if (i == 0) {
+        close(fds[0]);
+        eratostenes(fds_bis);
+        exit(0);
+    } else {
+        close(fds_bis[0]);
+        bool cerrado = false;
+        while (!cerrado) {
+            int num = 0;
+            int lectura = read(fds[0], &num, sizeof(num));
+            if (lectura < 0) {
+                perror("Error en la lectura");
+                exit(-1);
+            } else if (lectura == 0) {
+                cerrado = true;
+                close(fds[0]);
+            }
+
+            if ((num % prime) != 0) {
+                if (write(fds_bis[1], &num, sizeof(num)) < 0) {
+                    perror("Error en la escritura");
+                    exit(-1);
+                }
+            }
+        }
+        close(fds_bis[1]);
+        waitpid(i, NULL, 0);
+        exit(0);
+    }
 }
 
 
 int
 main(int argc, char *argv[])
 {
-	if (argc != 2) {
-		perror("Número incorrecto de argumentos");
-		return (-1);
-	}
-	long number = strtol(argv[1], NULL, 10);  //  Paso a base 10
-	int fds[2];
-	int result = pipe(fds);
-	if (result < 0) {
-		perror("Error en el pipe");
-	}
-	int f = fork();
-	if (f < 0) {
-		perror("Error en el fork");
-		return (-1);
-	}
-	if (f == 0) {  //  Hijo
-		erastotenes(fds);
-	} else {  //  Proceso generador
-		for (int i = 2; i < number; i++) {
-			int aux = i;
-			if (write(fds[1], &aux, sizeof(aux)) < 0) {
-				perror("Error en la escritura");
-				return (-1);
-			}
-		}
-		close(fds[1]);
-		close(fds[0]);
-		waitpid(f, NULL, 0);
-	}
-	exit(0);
+    if (argc != 2) {
+        perror("No recibió un número");
+        return -1;
+    }
+
+    int num = strtol(argv[1], NULL, 10);
+    int fds[2];
+    int res = pipe(fds);
+    if (res < 0) {
+        perror("Error en pipe");
+    }
+    int f = fork();
+    if (f < 0) {
+        perror("Error en fork");
+        exit(-1);
+    }
+    if (f == 0) {
+        eratostenes(fds);
+    } else {
+        for (int i = 2; i < num; i++) {
+            int j = i;
+            if (write(fds[1], &j, sizeof(j)) < 0) {
+                perror("Error en la escritura");
+                exit(-1);
+            }
+        }
+        close(fds[1]);
+        close(fds[0]);
+        waitpid(f, NULL, 0);
+    }
+    exit(0);
 }
