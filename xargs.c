6ce103cc5d29e317d65538d7b59abe5e1b15bf99
Franco <fcuppari@fi.uba.ar>
2023-03-27 16:21:30 -0300

    xargs working, tests passed with no memleaks

--- xargs.c
+++ xargs.c
@@ -1,61 +1,67 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/stat.h>
-#include <sys/wait.h>
-#include <string.h>
-
 #ifndef NARGS
 #define NARGS 4
 #endif
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
 
-
-void fork_proccess(char *commands[]);
+void execute(char *args[]);
+void clean_commands(char *args[], int i);
 
 void
-fork_proccess(char *commands[])
+execute(char *args[])
 {
-    pid_t pidC;
-    pidC = fork();
-    if (pidC < 0) {
-        perror("Error en el fork");
+    int i = fork();
+    if (i < 0) {
+        perror("Error en fork");
+        exit(-1);
     }
 
-    if (pidC > 0) {
+    if (i == 0) {
+        execvp(args[0], args);
+    } else {
         wait(NULL);
-    } else if (pidC == 0) {
-        execvp(commands[0], commands);
+    }
+}
+
+void
+clean_commands(char *args[], int i)
+{
+    for (int j = 1; j < i; j++) {
+        free(args[j]);
     }
 }
 
 int
 main(int argc, char *argv[])
 {
-    char *line = NULL;
-    size_t len = 0;
-
-    char *commands[NARGS + 2] = { argv[1], NULL, NULL, NULL, NULL, NULL };
-
     if (argc < 2) {
-        perror("Cantidad errónea de parámetros");
+        perror("Erronea cantidad de parametros");
         exit(EXIT_FAILURE);
     }
-
-    int iteracion = 1;
-
-    while (getline(&line, &len, stdin) != -1) {
-        line[strlen(line) - 1] = '\0';
-        commands[iteracion] = strdup(line);
-        if (iteracion == 4) {
-            fork_proccess(commands);
-            iteracion = 0;
+    size_t len = 0;
+    size_t read = 0;
+    char *line = NULL;
+    char *args[NARGS + 2] = { argv[1] };
+    int n_commands = 1;
+    while ((read = getline(&line, &len, stdin) != -1)) {
+        if (n_commands == (NARGS + 1)) {  // ya lei NARGS veces
+            args[n_commands] = NULL;
+            execute(args);
+            clean_commands(args, n_commands);
+            n_commands = 1;
         }
-
-        iteracion++;
+        line[strlen(line) - 1] = '\0';
+        args[n_commands] = strdup(line);
+        n_commands++;
     }
-
-    fork_proccess(commands);
+    if (n_commands > 1) {  //
+        args[n_commands] = NULL;
+        execute(args);
+        clean_commands(args, n_commands);
+    }
 
     free(line);
     exit(EXIT_SUCCESS);
